e5857c9da08173d99078c8792487aaff5cdfa11b
Daniel Dimitrov <dani2dim@yahoo.com>
2018-06-30 19:03:40 +0200

    Changed server to only serve 2 files, added a working nginx config with internal caching

--- nodejs-media-server/server.js
+++ nodejs-media-server/server.js
@@ -1,23 +1,33 @@
 const express = require("express");
 var bodyParser = require("body-parser");
 const app = express();
+var url = require("url");
 var path = require('path');
 var DIST_DIR = path.join(__dirname, "./src/");
 var d = new Date();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(DIST_DIR));
+//app.use("/src", express.static(DIST_DIR));
+
+app.get("/src/VNFManager.mp4", function (req, res) {
+    var d = new Date();
+    console.log("Video fetched at:", new Date().toLocaleString());
+
+    // max-age is in seconds
+    res.setHeader("Cache-Control", "public, max-age=60");
+    console.log(req.url);
+    res.sendFile(path.join(DIST_DIR, "VNFManager.mp4"));
+});
 
 app.get("*", function (req, res) {
     var d = new Date();
     console.log("Video fetched at:", new Date().toLocaleString());
-    res.setHeader("Cache-Control", "public, max-age=10");
-    res.setHeader("Expires", new Date(Date.now() + 1000).toUTCString());
 
-    console.log("HERE E",JSON.stringify(res.headers));
-    res.end("hello world");
-    //res.sendFile(path.join(DIST_DIR, "index.html"));
+    // max-age is in seconds
+    res.setHeader("Cache-Control", "public, max-age=60");
+    console.log(req.url);
+    res.sendFile(path.join(DIST_DIR, "index.html"));
 });
 
 app.listen(4312, function () {
